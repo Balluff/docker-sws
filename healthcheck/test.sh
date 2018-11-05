246d4b6b452dab34b8dac79330a5f2c1cadfbc66
Andreas Elser <andreas.elser@balluff.de>
2018-11-05 18:21:59 +0100

    Reorganized the project structure for better Docker tool chain possibilites. Changed the base image for the webserver from Ubuntu to Alpine. Optimized the Dockerfile to work lighttpd on Alpine Linux. Also added some information to the docker-compose files.

--- healthcheck/test.sh
+++ healthcheck/test.sh
@@ -5,7 +5,7 @@ sleep 5
 
 # Use curl to connect to the local webserver inside the container's network.
 # Search for the content inside the index.html.
-if curl web:3000 | grep 'My fast web container'; then
+if curl -f http://web:3000; then
     echo "Tests passed!"
     exit 0
 else
