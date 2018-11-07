b0e5376e5ece77e47a5b4da6f74ae3308db27b21
Andreas Elser <andreas.elser@balluff.de>
2018-11-07 08:24:40 +0100

    Modified the healthcheck test.sh to check the modified index.html

--- healthcheck/test.sh
+++ healthcheck/test.sh
@@ -5,10 +5,10 @@ sleep 5
 
 # Use curl to connect to the local webserver inside the container's network.
 # Search for the content inside the index.html.
-if curl -f http://web:3000; then
-    echo "Tests passed!"
+if curl http://web:3000/index.html | grep "<h3>Simple Webserver</h3>"; then
+    echo "Tests with curl and grep passed!"
     exit 0
 else
-    echo "Tests failed!"
+    echo "Tests with curl and grep failed!"
     exit 1
 fi
